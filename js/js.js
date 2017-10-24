0c1a0d333b9dd95970eb63c092ff7277faaaec39
Zakhar Shchipets <zshchipets@gmail.com>
2017-10-24 18:45:57 +0300

    Commit #4
    Main and modal check

--- js/js.js
+++ js/js.js
@@ -2,6 +2,21 @@ var slider = Peppermint(document.getElementById('peppermint'), {
   dots: true
 });
 
+var menuBtn = document.querySelector('.menu-btn'),
+		modal = document.querySelector('.modal');
+
+
+
+
+
+var openModal = () => {
+	'use strict';
+	modal.classList.toggle('modal--active');
+	document.querySelector('body').classList.toggle('body--noscroll');
+}
+
+
+
 
 
 
@@ -16,9 +31,7 @@ var changeInputLabel = (event) => {
 		if (target.value === '') {
 			target.previousElementSibling.classList.toggle('form-style__label--active');
 		}
-
 	}
-
 }
 
 
@@ -26,5 +39,9 @@ var changeInputLabel = (event) => {
 
 
 
+
+
+
+menuBtn.addEventListener('click', openModal);
 document.querySelector('.form-style__inner').addEventListener('focusin', changeInputLabel);
 document.querySelector('.form-style__inner').addEventListener('focusout', changeInputLabel);
