29b0b856482e37f19585bd4e1619ee0a3861dac1
Zakhar Shchipets <zshchipets@gmail.com>
2017-10-24 23:00:08 +0300

    Commit #5
    minor changes + title icon

--- js/js.js
+++ js/js.js
@@ -12,7 +12,12 @@ var menuBtn = document.querySelector('.menu-btn'),
 var openModal = () => {
 	'use strict';
 	modal.classList.toggle('modal--active');
+	document.querySelector('.hamburger').classList.toggle('hamburger--active');
 	document.querySelector('body').classList.toggle('body--noscroll');
+	document.querySelector('main').classList.toggle('modal-blur');
+	document.querySelector('footer').classList.toggle('modal-blur');
+	document.querySelector('.header__info').classList.toggle('modal-blur');
+	document.querySelector('.menu-btn__text').classList.toggle('menu-btn__text--active');
 }
 
 
@@ -36,7 +41,25 @@ var changeInputLabel = (event) => {
 
 
 
+var showSharesBtn1 = () => {
+	'use strict';
+	document.querySelector('.shares__link--1').classList.toggle('shares__link--active');
+}
 
+var showSharesBtn2 = () => {
+	'use strict';
+	document.querySelector('.shares__link--2').classList.toggle('shares__link--active');
+}
+
+var showSharesBtn3 = () => {
+	'use strict';
+	document.querySelector('.shares__link--3').classList.toggle('shares__link--active');
+}
+
+var showSharesBtn4 = () => {
+	'use strict';
+	document.querySelector('.shares__link--4').classList.toggle('shares__link--active');
+}
 
 
 
@@ -45,3 +68,11 @@ var changeInputLabel = (event) => {
 menuBtn.addEventListener('click', openModal);
 document.querySelector('.form-style__inner').addEventListener('focusin', changeInputLabel);
 document.querySelector('.form-style__inner').addEventListener('focusout', changeInputLabel);
+document.querySelector('.shares__bg--1').addEventListener('mouseover', showSharesBtn1);
+document.querySelector('.shares__bg--1').addEventListener('mouseout', showSharesBtn1);
+document.querySelector('.shares__bg--2').addEventListener('mouseover', showSharesBtn2);
+document.querySelector('.shares__bg--2').addEventListener('mouseout', showSharesBtn2);
+document.querySelector('.shares__bg--3').addEventListener('mouseover', showSharesBtn3);
+document.querySelector('.shares__bg--3').addEventListener('mouseout', showSharesBtn3);
+document.querySelector('.shares__bg--4').addEventListener('mouseover', showSharesBtn4);
+document.querySelector('.shares__bg--4').addEventListener('mouseout', showSharesBtn4);
