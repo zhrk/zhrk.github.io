0800545682eebc91d5aa59d98d8875f61fe180b0
Zakhar Shchipets <zshchipets@gmail.com>
2017-10-24 15:59:25 +0300

    Commit #3
    minor check

--- js/js.js
+++ js/js.js
@@ -1,3 +1,30 @@
 var slider = Peppermint(document.getElementById('peppermint'), {
   dots: true
 });
+
+
+
+
+
+
+var changeInputLabel = (event) => {
+	'use strict';
+	let target = event.target;
+
+	if (target.classList.contains('form-style__input')) {
+
+		if (target.value === '') {
+			target.previousElementSibling.classList.toggle('form-style__label--active');
+		}
+
+	}
+
+}
+
+
+
+
+
+
+document.querySelector('.form-style__inner').addEventListener('focusin', changeInputLabel);
+document.querySelector('.form-style__inner').addEventListener('focusout', changeInputLabel);
