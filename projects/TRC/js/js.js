2bb9c0b0609dbd4cc6fe346aa2ae33f6bb500d73
zhrk <zshchipets@gmail.com>
2018-01-07 02:50:52 +0300

    Commit #115
    hide header on scroll

--- projects/TRC/js/js.js
+++ projects/TRC/js/js.js
@@ -98,6 +98,42 @@ document.querySelector('.menu__close-btn').addEventListener('click', closeMenu);
 
 
 
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        $('header').removeClass('header--didnt-scrolled').addClass('header--scrolled');
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('header--scrolled').addClass('header--didnt-scrolled');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+
+
 
 
 
