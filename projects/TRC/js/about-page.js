7219ae71962c86114d8959dc44087f405027e258
zhrk <zshchipets@gmail.com>
2018-01-06 22:15:03 +0300

    Commit #108
    +about

---
+++ projects/TRC/js/about-page.js
@@ -0,0 +1,21 @@
+function aboutPageMobile() {
+	if (window.innerWidth < 767) {
+		$('.about-page__welcome-items').slick({
+		  infinite: true,
+		  autoplay: true,
+  		autoplaySpeed: 2000,
+		  arrows: false,
+		  dots: false,
+		  touchMove: true
+		});
+
+		$('.about-page__slides').slick({
+			infinite: false,
+			arrows: false,
+		  dots: true,
+		  touchMove: true
+		});
+	}
+}
+
+aboutPageMobile();
