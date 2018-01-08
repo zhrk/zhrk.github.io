ef06ac75b4120e93d596f57a8cd3a1189ad6c82e
zhrk <zshchipets@gmail.com>
2018-01-08 14:52:12 +0300

    Commit #120
    +mags

---
+++ projects/TRC/js/mags.js
@@ -0,0 +1,19 @@
+function openNav() {
+	document.querySelector('.mags__nav').classList.add('mags__nav--active');
+	document.querySelector('body').classList.add('body--noscroll');
+}
+
+function closeNav() {
+	document.querySelector('.mags__nav').classList.remove('mags__nav--active');
+	document.querySelector('body').classList.remove('body--noscroll');
+}
+
+
+
+
+
+
+
+
+document.querySelector('.mags__nav-open').addEventListener('click', openNav);
+document.querySelector('.mags__nav-close').addEventListener('click', closeNav);
