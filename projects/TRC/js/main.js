2d9561ce55f889d338f73cb3b476b38883a48591
zhrk <zshchipets@gmail.com>
2018-01-14 15:45:08 +0300

    Commit #143
    load fix

--- projects/TRC/js/main.js
+++ projects/TRC/js/main.js
@@ -30,59 +30,62 @@ function mainMobile() {
 	}
 }
 
+$(document).ready(function() {
 
-$('.welcome').slick({
-	  infinite: false,
-	  arrows: false,
-	  dots: true,
-	  touchMove: true
-	});
+  $('.welcome').slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    touchMove: true
+  });
 
-$('.ads__actual-2').slick({
-  infinite: false,
-  arrows: false,
-  dots: true,
-  touchMove: false,
-  responsive: [
-  {
-    breakpoint: 767,
-    settings: {
-    	arrows: false,
-    	centerMode: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerPadding: (window.innerWidth - 450) / 2 + 'px'
-    }
-  }
-  ]
+  $('.ads__actual-2').slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    touchMove: false,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: (window.innerWidth - 450) / 2 + 'px'
+        }
+      }
+    ]
+  });
+
+  $('.trends').slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    touchMove: false
+  });
+
+  $('.movie__list').slick({
+    infinite: false,
+    arrows: true,
+    touchMove: false,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          touchMove: true,
+          arrows: false,
+          centerMode: true,
+          centerPadding: (window.innerWidth - 225) / 2 + 'px',
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
+  mainMobile();
+  
 });
-
-$('.trends').slick({
-  infinite: false,
-  arrows: false,
-  dots: true,
-  touchMove: false
-});
-
-$('.movie__list').slick({
-  infinite: false,
-  arrows: true,
-  touchMove: false,
-  slidesToShow: 5,
-  slidesToScroll: 3,
-  responsive: [
-  {
-    breakpoint: 767,
-    settings: {
-    	touchMove: true,
-    	arrows: false,
-    	centerMode: true,
-    	centerPadding: (window.innerWidth - 225) / 2 + 'px',
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    }
-  }
-  ]
-});
-
-mainMobile();
