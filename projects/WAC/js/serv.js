d39ff0a7ccadfd53a1b94ad7bfb93de10a2974c3
Zakhar Shchipets <zshchipets@gmail.com>
2018-04-07 04:37:39 +0300

    Commit #210
    fixses

--- projects/WAC/js/serv.js
+++ projects/WAC/js/serv.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-	
-	$('#serv-nav').niceSelect();
+function mainMobile() {
+	if (window.innerWidth < 1280) {
 
-});
+    $(document).ready(function() {
+	
+			$('#serv-nav').niceSelect();
+
+		});
+
+
+}
+
+}
+
+mainMobile();
