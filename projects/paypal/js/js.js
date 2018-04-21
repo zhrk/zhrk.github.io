c0e7eaf446473bf212bb3d1373f3044ec3e6b486
Zakhar Shchipets <zshchipets@gmail.com>
2018-04-22 01:53:15 +0300

    Commit #220
    -paypal/video

--- projects/paypal/js/js.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('amount_input').addEventListener('input', changeAmount);
-
-function changeAmount(event) {
-	document.getElementById('form_amount').value = event.target.value;
-}
