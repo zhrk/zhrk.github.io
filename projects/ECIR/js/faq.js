91e5b2f499dbb17d336be6db6b1a06c6b8d741b0
Zakhar Shchipets <zshchipets@gmail.com>
2018-05-09 00:28:58 +0300

    Commit #228
    +faq
    nice commit № lul

---
+++ projects/ECIR/js/faq.js
@@ -0,0 +1,22 @@
+$(".faq__card-title").each(function() {
+  $(this).click(function() {
+    $(this).toggleClass("faq__card-title--visible");
+    $(this).next().toggleClass("faq__info--visible");
+  });
+});
+
+/*$('body').scrollspy({target: "#myScrollspy"})*/
+
+var startNavOffset = $(".faq__nav").offset()['top'];
+
+$(window).scroll(function() {
+  if ($(window).width() > 1279) {
+    if($(window).scrollTop() > startNavOffset) {
+      $(".faq__nav").addClass("faq__nav--fixed");
+      $(".faq__list").addClass("faq__list--fixed");
+    } else {
+      $(".faq__nav").removeClass("faq__nav--fixed");
+      $(".faq__list").removeClass("faq__list--fixed");
+    }
+  }
+});
