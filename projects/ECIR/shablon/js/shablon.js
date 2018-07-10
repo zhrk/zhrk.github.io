78ce86ced32ff744ff9f06ce42598b98e835e039
Zakhar Shchipets <zshchipets@gmail.com>
2018-07-10 21:35:20 +0300

    fix

--- projects/ECIR/shablon/js/shablon.js
+++ projects/ECIR/shablon/js/shablon.js
@@ -3,3 +3,16 @@ $(".rws__card-rate-bar").each(function(index) {
     theme: 'fontawesome-stars'
   });
 });
+
+
+document.querySelector('.shablon__nav-mobile > select').addEventListener('change', function() {
+
+  var cardList = document.querySelectorAll('.faq__list--shablon > .shablon__card-wrapper');
+
+  for (var i = 0; i < cardList.length; i++) {
+    cardList[i].classList.remove('active');
+  }
+
+
+  document.getElementById(this.value).classList.add('active');
+})
