f9b248cee50f13a6d908eddce3c49e84830a9d8c
Zakhar Shchipets <zshchipets@gmail.com>
2018-08-27 06:32:06 +0300

    +new

---
+++ projects/ECIR/js/new.js
@@ -0,0 +1,42 @@
+$.ionTabs("#tabs_1", {
+  type: "none"
+});
+
+$.ionTabs("#tabs_2", {
+  type: "none"
+});
+
+$('#theme').niceSelect();
+$('#lang').niceSelect();
+$('#country').niceSelect();
+$('#city').niceSelect();
+
+
+
+
+$(".new__live-more").click(function() {
+  $(".new__live-dop").toggleClass("new__live-dop--visible");
+});
+
+$(".ionTabs__tab--bots").click(function() {
+  $(".new__live").removeClass("add__list--visible");
+  $(".add__list").addClass("add__list--visible");
+});
+
+$(".ionTabs__tab--peoples").click(function() {
+  $(".add__list").removeClass("add__list--visible");
+  $(".new__live").addClass("new__live--visible");
+});
+
+
+
+
+$(".new__live-top-btn--rules").click(function() {
+  $(".new__live-details").removeClass("new__live-details--visible");
+  $(".new__live-rules").toggleClass("new__live-rules--visible");
+});
+
+$(".new__live-top-btn--details").click(function() {
+  $(".new__live-rules").removeClass("new__live-rules--visible");
+  $(".new__live-details").toggleClass("new__live-details--visible");
+});
