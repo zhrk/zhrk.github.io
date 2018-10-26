9b29c517365cfb794c1c16988d28e60959ae2e14
Zakhar Shchipets <zshchipets@gmail.com>
2018-10-26 15:21:44 +0300

    +parko

---
+++ projects/PARKO/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  $('#fullpage').fullpage({
+    navigation: true,
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    sectionSelector: '.welcome__section'
+  });
+
+  $(".welcome__slider").on("init", function(event, slick, currentSlide) {
+    $(this).append(
+      '<div class="slick-counter">'+ parseInt(slick.currentSlide + 1, 10) +' / '+ slick.slideCount +'</div>'
+    );
+  });
+
+  $(".welcome__slider").on("afterChange", function(event, slick, currentSlide) {
+    $(this).find('.slick-counter').html(slick.currentSlide + 1 + ' / ' + slick.slideCount);
+  });
+
+  $('.welcome__slider').slick({
+    draggable: false
+  });
+
+});
