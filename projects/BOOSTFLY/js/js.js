dae1d215e99d0e003e9575f55e32ca896bacbc14
Zakhar Shchipets <zshchipets@gmail.com>
2019-01-21 23:17:15 +0300

    move BOOSTFLY to separate repository

--- projects/BOOSTFLY/js/js.js
+++
@@ -1,7 +0,0 @@
-$(".header__menu").on('click', function(){
-  $(".header__nav-container").addClass("header__nav-container--active");
-})
-
-$(".header__close").on('click', function(){
-  $(".header__nav-container").removeClass("header__nav-container--active");
-})
